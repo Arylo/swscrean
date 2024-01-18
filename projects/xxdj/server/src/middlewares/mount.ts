88df95c6fc8aa5e86cbeb37a3e3768a070244c8d
AryloYeung <arylo.open@gmail.com>
2024-01-18 15:05:48 +0800

    feat: cache the static index file data

--- projects/xxdj/server/src/middlewares/mount.ts
+++ projects/xxdj/server/src/middlewares/mount.ts
@@ -1,5 +1,6 @@
 import { Middleware } from "koa"
 import { glob } from 'glob'
+import fs from 'fs'
 import path from 'path'
 import yn from 'yn'
 import KoaEtag from 'koa-etag'
@@ -8,32 +9,51 @@ import KoaStatic from 'koa-static'
 import md5File from 'md5-file'
 
 interface MountMWOptions {
-    prefix?: string,
-    path: string,
-    indexJSON?: boolean,
+  prefix?: string,
+  path: string,
+  indexJSON?: boolean,
 }
 
-export function Mount (opts: MountMWOptions) {
-    const {
-        prefix = '/',
-        path: folderPath,
-        indexJSON = false,
-    } = opts
-    const etagMw = KoaEtag()
-    const staticMw = KoaStatic(folderPath, { defer: true })
-    const mw: Middleware = async (ctx, next) => {
-        if (indexJSON && yn(ctx.query.index)) {
-            const list = glob.sync('**/*.*', { cwd: folderPath })
-            ctx.body = list.map((p) => {
-                return {
-                    md5sum: md5File.sync(path.resolve(folderPath, p)),
-                    path: path.resolve(prefix, p),
-                }
-            })
-            ctx.status = 200
-            return next()
-        }
-        await etagMw(ctx, () => staticMw(ctx, next))
+export function Mount(opts: MountMWOptions) {
+  const map: Record<string, { mtimeMs: number, md5sum: string }> = {}
+  const {
+    prefix = '/',
+    path: folderPath,
+    indexJSON = false,
+  } = opts
+  const etagMw = KoaEtag()
+  const staticMw = KoaStatic(folderPath, { defer: true })
+  function getMd5sum(filepath: string) {
+    const fileStat = fs.statSync(filepath)
+    const md5Obj = map[filepath]
+    const md5sum = md5Obj && md5Obj.mtimeMs === fileStat.mtimeMs ? md5Obj.md5sum : md5File.sync(filepath)
+    map[filepath] = {
+      mtimeMs: fileStat.mtimeMs,
+      md5sum,
     }
-    return KoaMount(prefix, mw)
+    return md5sum
+  }
+  const mw: Middleware = async (ctx, next) => {
+    if (indexJSON && yn(ctx.query.index)) {
+      const list = glob.sync('**/*.*', { cwd: folderPath })
+      ctx.body = list.map((p) => {
+        const filepath = path.resolve(folderPath, p)
+        return {
+          md5sum: getMd5sum(filepath),
+          path: path.resolve(prefix, p),
+        }
+      })
+      ctx.status = 200
+      return next()
+    }
+    await etagMw(ctx, () => staticMw(ctx, next))
+  }
+  if (indexJSON) {
+    const list = glob.sync('**/*.*', { cwd: folderPath })
+    list.forEach((p) => {
+      const filepath = path.resolve(folderPath, p)
+      getMd5sum(filepath)
+    })
+  }
+  return KoaMount(prefix, mw)
 }
